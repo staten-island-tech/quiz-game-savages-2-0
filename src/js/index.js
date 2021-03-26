1e6504240bb48ec0aabf7121ea924a1c4a6e51a0
rachhely <71524300+rachhely@users.noreply.github.com>
2021-03-26 02:54:15 -0400

    some basic js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,34 @@
-console.log("ajdslasjd")
+//Questions
+/* When is National Name Your Car Day?
+    Oct 2.
+When is National Frog Jumping Day
+    May 13.
+Which national food holiday is on March 2?
+    National Egg McMuffin Day
+What about Feb 7?
+    National Fettuccine Alfredo Day
+And When is National Be a Millionaire Day?
+    May 20. */
+
+[
+  {
+    questionContent: [
+      "When is National Name Your Car Day?",
+      "When is National Frog Jumping Day",
+      "Which national food holiday is on March 2?",
+      "What about Feb 7?",
+      "And When is National Be a Millionaire Day?",
+    ],
+
+    choices: [
+      {
+        choice: "Oct 2", //first question
+        correct: true,
+        value: 1,
+      },
+      { choice: "Jan 19", correct: false },
+      { choice: "Sept 30", correct: false },
+      { choice: "March 1", correct: false },
+    ],
+  },
+];
