b0ac5f8d6472df10451478e73ef6d614468866a8
vincent1598 <vincent.he0403@gmail.com>
2021-04-15 00:14:16 -0400

    working next button

--- src/js/index.js
+++ src/js/index.js
@@ -21,6 +21,10 @@ let totalScore = 0;
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
   //console.log("Started");
@@ -66,6 +70,12 @@ function selectAnswer(e) {
     totalScore++;
   }
   nextButton.classList.remove("hide");
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
 }
 
 const questions = [
@@ -76,6 +86,42 @@ const questions = [
       { text: "July 18", correct: false },
       { text: "Dec 29", correct: false },
       { text: "Feb 15", correct: false },
+    ],
+  },
+  {
+    question: "When is National Frog Jumping Day?",
+    answers: [
+      { text: "September 2", correct: false },
+      { text: "June 12", correct: false },
+      { text: "May 13", correct: true },
+      { text: "March 28", correct: false },
+    ],
+  },
+  {
+    question: "Which national food holiday is on March 2?",
+    answers: [
+      { text: "National Hot Dog Day", correct: false },
+      { text: "National Science Fiction Day", correct: false },
+      { text: "National Egg McMuffin Day", correct: true },
+      { text: "National Pet Day", correct: false },
+    ],
+  },
+  {
+    question: "What about Feb 7?",
+    answers: [
+      { text: "National Fettuccine Alfredo Day", correct: true },
+      { text: "National Trivia Day", correct: false },
+      { text: "Nothing Day", correct: false },
+      { text: "National Clean Off Your Desk Day", correct: false },
+    ],
+  },
+  {
+    question: "And When is National Be a Millionaire Day?",
+    answers: [
+      { text: "December 6", correct: false },
+      { text: "April 25", correct: false },
+      { text: "Dec 2", correct: false },
+      { text: "May 20", correct: true },
     ],
   },
 ];
