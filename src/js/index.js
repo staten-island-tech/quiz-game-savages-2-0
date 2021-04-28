531dcc5e343e5a1fb96aed2f3c37fe3eccd616cf
vincent1598 <vincent.he0403@gmail.com>
2021-04-27 23:06:30 -0400

    revisions

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,8 @@ What about Feb 7?
 And When is National Be a Millionaire Day?
     May 20. */
 
+import { questions } from "./questions";
+
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const quizTitle = document.getElementById("quiz-title");
@@ -92,51 +94,3 @@ function showResults() {
   resultsHeading.classList.remove("hide");
   resultsValue.classList.remove("hide");
 }
-
-const questions = [
-  {
-    question: "When is National Name Your Car Day?",
-    answers: [
-      { text: "Oct 2", correct: true },
-      { text: "July 18", correct: false },
-      { text: "Dec 29", correct: false },
-      { text: "Feb 15", correct: false },
-    ],
-  },
-  {
-    question: "When is National Frog Jumping Day?",
-    answers: [
-      { text: "September 2", correct: false },
-      { text: "June 12", correct: false },
-      { text: "May 13", correct: true },
-      { text: "March 28", correct: false },
-    ],
-  },
-  {
-    question: "Which national food holiday is on March 2?",
-    answers: [
-      { text: "National Hot Dog Day", correct: false },
-      { text: "National Science Fiction Day", correct: false },
-      { text: "National Egg McMuffin Day", correct: true },
-      { text: "National Pet Day", correct: false },
-    ],
-  },
-  {
-    question: "What about Feb 7?",
-    answers: [
-      { text: "National Fettuccine Alfredo Day", correct: true },
-      { text: "National Trivia Day", correct: false },
-      { text: "Nothing Day", correct: false },
-      { text: "National Clean Off Your Desk Day", correct: false },
-    ],
-  },
-  {
-    question: "And When is National Be a Millionaire Day?",
-    answers: [
-      { text: "December 6", correct: false },
-      { text: "April 25", correct: false },
-      { text: "Dec 2", correct: false },
-      { text: "May 20", correct: true },
-    ],
-  },
-];
